83babcd27da36abdeeb908fa8c1633c28ca86a75
schueler <jordyrein@yahoo.com>
2025-01-16 17:22:17 +0100

    Kunde Info Editor anpassen

--- src/www/Admin.js
+++ src/www/Admin.js
@@ -1,23 +1,42 @@
+import Customer from "/funrest/Customer.js"
+import Reservation from "/funrest/Reservation.js"
+
+const custCol=2;
+
 
 const newCustButton=document.getElementById("neu_kunde");
 const newReserveButton=document.getElementById("neu_buchung");
 
-//newCustButton.AddEventListener("click", ()=>{
-//  console.log("i am customer button");
-//  return;
-//});
-//
-//
-//newReserveButton.AddEventListener("click", ()=>{
-//
-//  console.log("i am reservation button");
-//  return;
-//});
 
 newCustButton.onclick=()=>{
   var infotable=document.getElementById("info");
   infotable.innerHTML = "";
+  var cust=new Customer("","","","","","");
   
+  //for (var i=0;i<custCol;++i){
+  //  var th=document.createElement("th");
+  //  var label=document.createElement("label");
+  //}
+
+  for(var prop in cust){ 
+    if(Object.prototype.hasOwnProperty.call(cust,prop)){
+      console.log(prop);
+      var tr=document.createElement("tr");
+      var td=document.createElement("td");
+      var label=document.createElement("label");
+      label.innerHTML=prop;
+
+      var input=document.createElement("input");
+      input.type="text";
+
+      td.appendChild(label);
+      td.appendChild(input);
+      tr.appendChild(td);
+
+      infotable.appendChild(tr);
+    }
+  }
+
 
   
 };
@@ -25,6 +44,5 @@ newCustButton.onclick=()=>{
 newReserveButton.onclick=()=>{
   var infotable=document.getElementById("info");
   infotable.innerHTML = "";
-
 
 };
---
+++ src/www/Customer.js
@@ -0,0 +1,10 @@
+export class Customer{
+  constructor(fname,lname, address, plz, loc, email){
+    this.fname=fname;
+    this.lname=lname;
+    this.address=address;
+    this.plz=plz;
+    this.loc=loc;
+    this.email=email;
+  }
+}
