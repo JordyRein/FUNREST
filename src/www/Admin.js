483448349e89211f3df7f99fbbc417ea3eb119e9
schueler <jordyrein@yahoo.com>
2025-01-16 14:16:37 +0100

    button function v1

---
+++ src/www/Admin.js
@@ -0,0 +1,30 @@
+
+const newCustButton=document.getElementById("neu_kunde");
+const newReserveButton=document.getElementById("neu_buchung");
+
+//newCustButton.AddEventListener("click", ()=>{
+//  console.log("i am customer button");
+//  return;
+//});
+//
+//
+//newReserveButton.AddEventListener("click", ()=>{
+//
+//  console.log("i am reservation button");
+//  return;
+//});
+
+newCustButton.onclick=()=>{
+  var infotable=document.getElementById("info");
+  infotable.innerHTML = "";
+  
+
+  
+};
+
+newReserveButton.onclick=()=>{
+  var infotable=document.getElementById("info");
+  infotable.innerHTML = "";
+
+
+};
